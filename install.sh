fbf12db108223c410489ee31b15fc90e41ebdba8
jorgee00 <jorge.rios.marfil@gmail.com>
2023-02-05 10:45:51 +0100

    dockerized

--- install.sh
+++ install.sh
@@ -4,15 +4,11 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-apt-cache policy docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo usermod -aG docker ${USER}
 
-curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-chmod +x ~/.docker/cli-plugins/docker-compose
-
-
 wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
 rm go1.19.4.linux-amd64.tar.gz
 
 echo export PATH=$PATH:/usr/local/go/bin >> $HOME/.bashrc
@@ -28,13 +24,13 @@ sudo apt-get install -y nodejs
 
 sudo apt-get install -y openjdk-11-jdk maven
 
-wget https://services.gradle.org/distributions/gradle-6.3-bin.zip -P /tmp –quiet
+wget https://services.gradle.org/distributions/gradle-6.3-bin.zip -P /tmp --quiet
 sudo unzip -q /tmp/gradle-6.3-bin.zip -d /opt && rm /tmp/gradle-6.3-bin.zip
 sudo ln -s /opt/gradle-6.3/bin/gradle /usr/bin
 
-git clone https://github.com/hyperledger/fabric.git -b release-2.2
+cd ~/go/src/github.com/hyperledger
 
-cd 
+git clone https://github.com/hyperledger/fabric.git -b release-2.2
 
 cd fabric
 make clean-all
